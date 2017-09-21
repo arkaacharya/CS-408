88d143d8404bac745e931278fd97b16338258821
arkaacharya <arkaacharya@outlook.com>
2017-09-21 18:04:36 -0400

    Add files via upload

---
+++ errorLogin.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<!-- Name on tab of the page -->
+	<title>Error Login</title>
+	
+	<!-- Redirecting after the timer -->
+	<meta http-equiv="refresh" content= "2; url='login.php'"/>
+<head>
+<body>
+<form>
+	<!-- Displaying the error message -->
+	<center><header><font size="+2" face="arial"><h1>Error Logging in. </br>Please try again.</h1></font></header></center>
+</form>
+
+</body>
+</html>
