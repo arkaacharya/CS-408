5671ec65442f0a78bcf18116c8dafe0665315557
Joyce Kao <kao5@purdue.edu>
2017-09-22 17:19:56 -0400

    added userAlreadyExists.php

---
+++ userAlreadyExists.php
@@ -0,0 +1,48 @@
+<?php
+	$servername = "localhost"; //Name of the server
+	$dbname = "OnTheExamLine"; //Name of the database
+	$username = "root"; //Username used to connect to the database
+	$password = NULL; //Password used to connect to the database
+
+	$conn = new mysqli($servername, $username, $password, $dbname); //Connecting to the database
+	if($conn->error){ //Checking connection for errors
+		die("Could not establish connection to database."); //Terminating this page
+	}
+	
+	$acessor = $_GET['acessor']; //Getting the value of acessor
+	
+	$sql = "SELECT isLoggedIn FROM users WHERE username=\"admin\""; //Constructing an sql query to get the login value of admin
+	$data = mysqli_query($conn, $sql); //Executing the sql query
+	$result1 = mysqli_fetch_row($data); //Extracting information from the executed query
+	
+	$isLoggedIn = false; //Storing the information in a new variable
+	
+	if($result1[0]){
+		$isLoggedIn = true;
+	}
+	
+	if($isLoggedIn && $acessor=='admin'){ //Checking the conditions to display the webpage
+?>
+	<html>
+	<head>
+		<!-- Name on tab of page -->
+		<title>User Already Exists</title>
+		
+		<!-- Redirecting after a timer -->
+		<meta http-equiv="refresh" content= "2; url='adminPage.php?acessor=admin'"/>
+	<head>
+	<body>
+	<form>
+		<!-- Displaying error message -->
+		<font size="+2" face="arial"><center><header><h1>Username already exists. Please try a different name.</h1></header></center></font>
+	</form>
+
+	</body>
+	</html>
+<?php
+	}
+	else{
+		header("Location: login.php"); //Redirecting to the login page
+		die; //Terminating this page
+	}
+?>
