327ea0bb0dcaafb4debd69cf9f247066ed09383e
shivaganesh96 <sbalasun@purdue.edu>
2017-09-22 17:27:53 -0400

    Add files via upload

---
+++ delTest.php
@@ -0,0 +1,94 @@
+<?php
+	$servername = "localhost"; //Name of the server
+	$dbname = "OnTheExamLine"; //Name of the database
+	$username = "root"; //Username used to connect to the database
+	$password = NULL; //Password used to connect to the database
+
+	$conn = new mysqli($servername, $username, $password, $dbname); //Connecting to the database
+	if($conn->error){ //Checking connection for errors
+		die("Could not establish connection to database."); //Terminating this page
+	}
+	
+	$acessor = $_GET['acessor']; //Getting the value of acessor
+	
+	$sql = "SELECT isLoggedIn FROM users WHERE username=\"admin\""; //Constructing an sql query to get the login value of admin
+	$data = mysqli_query($conn, $sql); //Executing the sql query
+	$result1 = mysqli_fetch_row($data); //Extracting information from the executed query
+	
+	$isLoggedIn = false; //Storing the information in a new variable
+	
+	if($result1[0]){
+		$isLoggedIn = true;
+	}
+	
+	if($isLoggedIn && $acessor=='admin'){ //Checking the conditions to display the webpage
+?>
+	<html>
+	<head>
+	<title>Delete Test</title> <!-- Name on tab of page -->
+
+	</head>
+	<body>
+	<form
+	action = "removeTest.php"
+	method = "post"
+	>
+	<!-- Title of page -->
+	<font size="+2" face="arial"><center><header><h1>Delete Existing Test</h1></header></center></font>
+
+	<div id="insideBody">
+	
+	<table border = "0">
+	<!-- Displaying all the test names in a group of radio buttons -->
+	<tr><td>
+		<font size="+2" face="arial">Select Test:</font size="+2"></td></tr>
+		<?php
+			//Constructing an sql query to get the name of the exam
+			$sql = "SELECT chapter FROM chapters";
+			$data1 = mysqli_query($conn, $sql); //Executing the sql query
+			$result1 = mysqli_fetch_row($data1); //Extracting information from the executed query
+			while($result1){ //Condition to loop as long as information is being received
+				$sql = "SELECT testName FROM ".$result1[0];
+				$data = mysqli_query($conn, $sql); //Executing the sql query
+				$result = mysqli_fetch_row($data); //Extracting information from the executed query
+				while($result){
+		?>
+	<tr><td>
+		<!-- Displaying the test name with a radio button -->
+		</br><input type = "radio" name = "testName" value = "<?php echo $result[0] ?>"/><font size="+2" face="arial"><?php echo $result[0] ?></font size="+2">
+	</td></tr>
+
+	<?php
+					$result = mysqli_fetch_row($data); //Extracting information from the executed query
+				}
+				$result1 = mysqli_fetch_row($data1); //Extracting information from the executed query
+			}
+	?>
+	
+	<!-- Button to submit the selected test -->
+	<tr>
+		<td
+		colspan = "2"
+		align  = "center">
+			<input type = "submit"
+			value = "Delete Test" /></td>
+	</tr>
+	</table>
+		<!-- Redirecting to the previous page -->
+		<font size="+2" face="arial"><a href="adminPage.php?acessor=admin">Back</a></font>
+		</br></br>
+	<font size="+2" face="arial"><a href="adminPage.php?acessor=admin">Home</a></font size="+2">
+		</br></br>
+		<font size="+2" face="arial"><a href="logout.php?userName=admin">Logout</a></font>
+
+	</form>
+
+	</body>
+	</html>
+<?php
+	}
+	else{
+		header("Location: login.php"); //Redirecting to the login page
+		die; //Terminating this page
+	}
+?>
